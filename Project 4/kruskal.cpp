793e747acc154965089e4f685b2cae6702cf4c4e
DoodleBin <alexahatcher4@gmail.com>
2025-11-14 18:58:35 -0600

    tsp and attempted kruskals

--- Project 4/kruskal.cpp
+++ Project 4/kruskal.cpp
@@ -14,33 +14,52 @@ using namespace std;
 
 /*
  * find
+ * given the vertex we check the vectopr pi to find where it is
+ * returns the index of the set that v belongs to
  */
-int find(int v, vector<int> &pi) {
-    return -1;
+int find(int v, vector<int> &pi)
+{
+    int sz = pi.size();// get size
+    for (int i = 0; i < sz; i++)
+    {
+        if (pi[i] == v)
+        {
+            return i;
+        }
+    }
+    return -1; //if its not in pi
 }
 
 /*
  * union_by_rank
  */
-void union_by_rank(int u, int v, vector<int> &rank, vector<int> &pi) {
+void union_by_rank(int u, int v, vector<int> &rank, vector<int> &pi)
+{
+    // int root1 = find(u, pi);
+    // int root2 = find(v, pi);
+    // if()
+    //  //connect both roots
+
     return;
 }
 
 /*
  * kruskal
  */
-vector<Edge> kruskal(vector<Vertex> &adjList, vector<Edge> &edgeList) {
+vector<Edge> kruskal(vector<Vertex> &adjList, vector<Edge> &edgeList)
+{
     // Initialize the empty MST.
     vector<Edge> mst;
-    
+
     // For each vertex, we need rank values and pi pointers (ints) for the
     // Disjoint Sets. Initialize the rank to 0 and pi to be the vertex labels.
     int n = adjList.size();
     vector<int> rank(n, 0);
     vector<int> pi(n);
-    for (int i=0; i<pi.size(); i++) {
-        pi[i] = i;
-    }
+    // for (int i = 0; i < pi.size(); i++)
+    // {
+    //     pi[i] = i;
+    // }
 
     return mst;
 }
--- Project 4/tsp.cpp
+++ Project 4/tsp.cpp
@@ -18,10 +18,23 @@ using namespace std;
 vector<int> tsp(vector<Vertex> &adjList, Vertex &start) {
     // Create the empty tour.
     vector<int> tour;
-    
+
     // Create the vector for tracking visited during the DFS.
     int n = adjList.size();
     vector<bool> visited(n, false);
+
+    Vertex current = start;
+    visited[current.label] = true;
+    for(int j = 0; j < n-1; j++){
+        current = adjList[j];
+        visited[current.label] = true;
+
+        for (int i : adjList[current.label].neighbors){
+            if (visited[i] == false){
+                tour.push_back(i);
+            } // visit all unvisited neighbors
+        }
+    }
 
     return tour;
 }
